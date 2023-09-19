928fefe4fed2216d77d74cc09f859e629f12eee3
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2023-09-19 15:45:08 -0500

    Update SparkWebServer.java

--- src/main/java/edu/eci/arep/SparkWebServer.java
+++ src/main/java/edu/eci/arep/SparkWebServer.java
@@ -10,7 +10,7 @@ public class SparkWebServer {
         get("sin", (req,res) -> AppService.getSin(req.queryParams("number")));
         get("palindrom", (req,res) -> AppService.isPalindrome(req.queryParams("palindrome")));
         get("magnitude", (req,res) -> AppService.getMagnitude(req.queryParams("x"),req.queryParams("y")));
-        HTTPServer.getInstance().start(args);
+        //HTTPServer.getInstance().start(args);
     }
 
     private static int getPort() {
